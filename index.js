6abf5aa5fd4145849c5a874b8f1f0c7f4298db1e
Pere <pere.solaclaver@gmail.com>
2020-04-06 15:37:34 +0100

    Create index file and modify script

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-require('./bot');
-require('./server');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Hello from the Bot API.' });
+});
+
+app.listen(port, () => {
+	console.log(`\n\nServer running on port ${port}.\n\n`);
+});
+
+module.exports = app;
